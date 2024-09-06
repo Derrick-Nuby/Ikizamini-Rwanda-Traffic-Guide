7ee9bfd0f848fc1a270f1d84c5ead529121c6dd2
Derrick [Nuby] Iradukunda <72933285+Derrick-Nuby@users.noreply.github.com>
2024-09-06 15:15:10 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white py-4 text-center">
             <p className="text-sm">
-                Developed by <a href="https://www.linkedin.com/in/ntezi/" className="text-blue-400 hover:underline" target="_blank" rel="noopener noreferrer">Marius Ngaboyamahina</a>
+                Cloned from ntezi Derrick NUBY
             </p>
         </footer>
     );
